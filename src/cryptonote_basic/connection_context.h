adb8424868187c12e87895e703a3a822e4b5f39a
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <president@worldvaporexpo.com>
2025-06-12 19:16:28 -0400

    Update connection_context.h

--- src/cryptonote_basic/connection_context.h
+++ src/cryptonote_basic/connection_context.h
@@ -28,133 +28,86 @@
 // 
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 // Parts of this file are originally copyright (c) 2006-2013, Andrey N. Sabelnikov, www.sabelnikov.net
-
 #pragma once
 #include <unordered_set>
 #include <atomic>
-#include <algorithm>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/optional/optional_fwd.hpp>
 #include "net/net_utils_base.h"
-#include "crypto/hash.h"
+#include "copyable_atomic.h"
 
 namespace cryptonote
 {
-  struct cryptonote_connection_context: public epee::net_utils::connection_context_base
+
+  struct cryptonote_connection_context : public epee::net_utils::connection_context_base
   {
-    cryptonote_connection_context(): m_state(state_before_handshake), m_remote_blockchain_height(0), m_last_response_height(0),
-        m_expected_heights_start(0), m_last_request_time(boost::date_time::not_a_date_time), m_callback_request_count(0),
-        m_last_known_hash(crypto::null_hash), m_pruning_seed(0), m_rpc_port(0), m_rpc_credits_per_hash(0), m_anchor(false), m_score(0),
-        m_expect_response(0), m_expect_height(0), m_num_requested(0) {}
+    cryptonote_connection_context() :
+      m_state(state_before_handshake),
+      m_remote_blockchain_height(0),
+      m_last_response_height(0),
+      m_last_request_time(boost::posix_time::microsec_clock::universal_time()),
+      m_callback_request_count(0),
+      m_last_known_hash(crypto::null_hash)
+    {}
+
+    // 🧩 Explicitly define copy constructor
+    cryptonote_connection_context(const cryptonote_connection_context& other)
+      : epee::net_utils::connection_context_base(other),
+        m_state(other.m_state),
+        m_needed_objects(other.m_needed_objects),
+        m_requested_objects(other.m_requested_objects),
+        m_remote_blockchain_height(other.m_remote_blockchain_height),
+        m_last_response_height(other.m_last_response_height),
+        m_last_request_time(other.m_last_request_time),
+        m_callback_request_count(other.m_callback_request_count.load()),
+        m_last_known_hash(other.m_last_known_hash)
+    {}
+
+    // 🧩 Explicitly define assignment operator
+    cryptonote_connection_context& operator=(const cryptonote_connection_context& other)
+    {
+      if (this != &other)
+      {
+        epee::net_utils::connection_context_base::operator=(other);
+        m_state = other.m_state;
+        m_needed_objects = other.m_needed_objects;
+        m_requested_objects = other.m_requested_objects;
+        m_remote_blockchain_height = other.m_remote_blockchain_height;
+        m_last_response_height = other.m_last_response_height;
+        m_last_request_time = other.m_last_request_time;
+        m_callback_request_count = other.m_callback_request_count.load();
+        m_last_known_hash = other.m_last_known_hash;
+      }
+      return *this;
+    }
 
     enum state
     {
-      state_before_handshake = 0, //default state
+      state_before_handshake = 0,
       state_synchronizing,
       state_standby,
       state_idle,
       state_normal
     };
 
-    /*
-      This class was originally from the EPEE module. It is identical in function to std::atomic<uint32_t> except
-      that it has copy-construction and copy-assignment defined, which means that earliers devs didn't have to write
-      custom copy-contructors and copy-assingment operators for the outer class, cryptonote_connection_context.
-      cryptonote_connection_context should probably be refactored because it is both trying to be POD-like while
-      also (very loosely) controlling access to its atomic members.
-    */
-    class copyable_atomic: public std::atomic<uint32_t>
-    {
-    public:
-      copyable_atomic()
-      {};
-      copyable_atomic(uint32_t value)
-      { store(value); }
-      copyable_atomic(const copyable_atomic& a):std::atomic<uint32_t>(a.load())
-      {}
-      copyable_atomic& operator= (const copyable_atomic& a)
-      {
-        store(a.load());
-        return *this;
-      }
-      uint32_t operator++()
-      {
-        return std::atomic<uint32_t>::operator++();
-      }
-      uint32_t operator++(int fake)
-      {
-        return std::atomic<uint32_t>::operator++(fake);
-      }
-    };
-
-    static constexpr int handshake_command() noexcept { return 1001; }
-    bool handshake_complete() const noexcept { return m_state != state_before_handshake; }
-
-    //! \return Maximum number of bytes permissible for `command`.
-    static size_t get_max_bytes(int command) noexcept;
-
-    //! Use this instead of `m_state = state_normal`.
-    void set_state_normal();
-
-    boost::optional<crypto::hash> get_expected_hash(uint64_t height) const;
-
     state m_state;
-    std::vector<std::pair<crypto::hash, uint64_t>> m_needed_objects;
-    std::vector<crypto::hash> m_expected_heights;
+    std::list<crypto::hash> m_needed_objects;
     std::unordered_set<crypto::hash> m_requested_objects;
     uint64_t m_remote_blockchain_height;
     uint64_t m_last_response_height;
-    uint64_t m_expected_heights_start;
     boost::posix_time::ptime m_last_request_time;
-    copyable_atomic m_callback_request_count; //in debug purpose: problem with double callback rise
+    epee::copyable_atomic m_callback_request_count;
     crypto::hash m_last_known_hash;
-    uint32_t m_pruning_seed;
-    uint16_t m_rpc_port;
-    uint32_t m_rpc_credits_per_hash;
-    bool m_anchor;
-    int32_t m_score;
-    int m_expect_response;
-    uint64_t m_expect_height;
-    size_t m_num_requested;
-    copyable_atomic m_new_stripe_notification{0};
-    copyable_atomic m_idle_peer_notification{0};
   };
 
   inline std::string get_protocol_state_string(cryptonote_connection_context::state s)
   {
     switch (s)
     {
-    case cryptonote_connection_context::state_before_handshake:
-      return "before_handshake";
-    case cryptonote_connection_context::state_synchronizing:
-      return "synchronizing";
-    case cryptonote_connection_context::state_standby:
-      return "standby";
-    case cryptonote_connection_context::state_idle:
-      return "idle";
-    case cryptonote_connection_context::state_normal:
-      return "normal";
-    default:
-      return "unknown";
-    }    
-  }
-
-  inline char get_protocol_state_char(cryptonote_connection_context::state s)
-  {
-    switch (s)
-    {
-    case cryptonote_connection_context::state_before_handshake:
-      return 'h';
-    case cryptonote_connection_context::state_synchronizing:
-      return 's';
-    case cryptonote_connection_context::state_standby:
-      return 'w';
-    case cryptonote_connection_context::state_idle:
-      return 'i';
-    case cryptonote_connection_context::state_normal:
-      return 'n';
-    default:
-      return 'u';
+      case cryptonote_connection_context::state_before_handshake: return "state_before_handshake";
+      case cryptonote_connection_context::state_synchronizing:    return "state_synchronizing";
+      case cryptonote_connection_context::state_standby:          return "state_standby";
+      case cryptonote_connection_context::state_idle:             return "state_idle";
+      case cryptonote_connection_context::state_normal:           return "state_normal";
+      default:                                                    return "unknown";
     }
   }
 
